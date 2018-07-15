2a92dcc9fed44d5974875a4c8374492f4f5b6252
Elaine Anderson <eparie@bu.edu>
2018-07-15 17:35:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+  erb :newteam
+end
+
+# Add your post route and action below
+#post '/food' do
+#  "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+#end
+
+
 
 end
